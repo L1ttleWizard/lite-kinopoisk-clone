ea959602f4cb2fb1f07927db70af6eefecb69541
l1ttlewizard <andrwyandreev2005@gmail.com>
2025-07-11 23:17:45 +0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/lite-kinopoisk-clone/'
+  base:'l1ttlewizard.github.io/lite-kinopoisk-clone/'
 })
